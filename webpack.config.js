08cd3f55001acfdcf622f27bf7bb4086afd51ebc
shimozurdo <iscjosenunezgarcia@gmail.com>
2020-12-29 10:04:13 -0600

    added webpack and babel config for creating a bundle

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = (env) => {
+
+    return {
+        entry: './main.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'bundle.js'
+        },        
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                }              
+            ]
+        }
+    }
+
+};
